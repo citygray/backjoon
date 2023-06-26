22aa1e2f4cf542899c5fd417fe95477859ce7fc1
DESKTOP-VCEIJH2\user1 <nanee12@naver.com>
2023-06-26 17:45:17 +0900

    feat: [1차원 배열] 1 개수 세기 - 작성중

---
+++ src/step04/Ex01_10807.java
@@ -0,0 +1,13 @@
+package step04;
+
+public class Ex01_10807 {
+
+	public static void main(String[] args) {
+		// 10807 - 개수 세기
+		
+		//첫번째줄 : 입력받을 정수의 개수
+		//두번째줄 : 개수만큼 받은 정수 (-100<- X <= 100)
+		//세번째줄 : 찾을려는 정수 (-100<- X <= 100)
+	}
+
+}
