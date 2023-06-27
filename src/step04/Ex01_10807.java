5d338e77f5f0d4d9fb68ed7b1bf0dd3298d76ef4
DESKTOP-VCEIJH2\user1 <nanee12@naver.com>
2023-06-27 17:41:04 +0900

    feat: [1차원 배열] 1 개수 세기 - 작성중(입력완료)

--- src/step04/Ex01_10807.java
+++ src/step04/Ex01_10807.java
@@ -1,13 +1,38 @@
 package step04;
 
-public class Ex01_10807 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
+public class Ex01_10807 {
+	
+	public static void main(String[] args) throws IOException {
 		// 10807 - 개수 세기
 		
 		//첫번째줄 : 입력받을 정수의 개수
 		//두번째줄 : 개수만큼 받은 정수 (-100<- X <= 100)
 		//세번째줄 : 찾을려는 정수 (-100<- X <= 100)
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		//첫번째줄 : 입력받을 정수의 개수 X
+		int X = Integer.parseInt(br.readLine());
+		
+		//두번째줄 : X개수만큼 받은 정수 (-100<- X <= 100)
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0;i<X;i++) {
+			sb.append(st.nextToken()).append(" ");
+		}
+		//세번째줄 : 찾을려는 정수 (-100<- X <= 100)
+		int Y = Integer.parseInt(br.readLine());
+		
+		
+		System.out.println("----------------------");
+		System.out.println("입력받을 정수의 개수: "+X);
+		System.out.println("입력한 정수"+sb.toString());
+		System.out.println("찾을려는 정수: "+Y);
 	}
 
 }
