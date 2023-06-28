69c26721fce3980fa9d5624da370e15a96eb3211
DESKTOP-VCEIJH2\user1 <nanee12@naver.com>
2023-06-28 10:35:49 +0900

    feat: [1차원 배열] 1 개수 세기 - 맞았습니다

--- src/step04/Ex01_10807.java
+++ src/step04/Ex01_10807.java
@@ -3,6 +3,7 @@ package step04;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Ex01_10807 {
@@ -10,29 +11,30 @@ public class Ex01_10807 {
 	public static void main(String[] args) throws IOException {
 		// 10807 - 개수 세기
 		
-		//첫번째줄 : 입력받을 정수의 개수
-		//두번째줄 : 개수만큼 받은 정수 (-100<- X <= 100)
-		//세번째줄 : 찾을려는 정수 (-100<- X <= 100)
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		int[] arr;
 		
 		//첫번째줄 : 입력받을 정수의 개수 X
 		int X = Integer.parseInt(br.readLine());
+		arr = new int[X];
 		
 		//두번째줄 : X개수만큼 받은 정수 (-100<- X <= 100)
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0;i<X;i++) {
-			sb.append(st.nextToken()).append(" ");
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		//세번째줄 : 찾을려는 정수 (-100<- X <= 100)
 		int Y = Integer.parseInt(br.readLine());
+		int count =0;
+		for(int num:arr) {
+			if(num==Y) count++;
+		} 
 		
 		
 		System.out.println("----------------------");
 		System.out.println("입력받을 정수의 개수: "+X);
-		System.out.println("입력한 정수"+sb.toString());
+		System.out.println("입력한 정수"+Arrays.toString(arr));
 		System.out.println("찾을려는 정수: "+Y);
+		System.out.println("정수 중에 Y가 몇 개인지 "+count);
 	}
-
 }
