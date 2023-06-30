1abebd3f8bd09bc384987b4ed854ba7066f59d73
DESKTOP-VCEIJH2\user1 <nanee12@naver.com>
2023-06-30 17:31:11 +0900

    feat: [1차원 배열] [05] [10812]  공넣기 (작성중)

---
+++ src/step04/Ex05_10810.java
@@ -0,0 +1,24 @@
+package step04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex05_10810 {
+
+	public static void main(String[] args) throws IOException{
+		// 10810 - 공 넣기
+		//입력
+		//정수 N,M 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		
+
+	}
+
+}
