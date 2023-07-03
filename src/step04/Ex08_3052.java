1b8d469ddcbe8f975bf02a1c9fe325a155aa5ded
DESKTOP-VCEIJH2\user1 <nanee12@naver.com>
2023-07-03 17:48:53 +0900

    feat: [배열][3052] 8 - 나머지 (작성중)

--- src/step04/Ex08_3052.java
+++ src/step04/Ex08_3052.java
@@ -1,9 +1,31 @@
 package step04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Ex08_3052 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// 3052 - 나머지
+		int count = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[10];
+		for(int i =0; i<10;i++) {
+			arr[i]= Integer.parseInt(br.readLine())%42;
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		Arrays.parallelSort(arr);
+		
+		for(int i = 0; i<10;i++) {
+			if(Arrays.binarySearch(arr,arr[i] )>0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		
 	}
 
 }
